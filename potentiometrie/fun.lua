a5a6c19ed64c5639754922bc06e4d3fc0722ff13
Nevroz Arslan <newrozarslan@gmail.com>
2016-11-27 20:59:31 +0100

    abgabe

--- potentiometrie/fun.lua
+++ potentiometrie/fun.lua
@@ -1,24 +1,27 @@
 function ph( V )
- print (2.5 - 0.01*V,50+(0.1*V),(2.5 - 0.01*V)  / (50+(0.1*V)) , math.log10((2.5 - 0.01*V)  / (50+(0.1*V))))
-  return -math.log10((2.5 - 0.01*V)  / (50+(0.1*V)))
+  return string.format("%.3f",(-math.log10((2.5 - 0.1*(V-50)) / V)))
 end
 function ph2( V )
-
-  return 14+math.log10((0.01*(V-250))  / (75+0.1*(V-250)))
+  return string.format("%.3f",14+math.log10(((V-75)/10) / V))
 end
-
-
-local f = io.open("naoht.txt", "w")
+--
+--local f = io.open("naoht.txt", "w")
+local f = io.open("tho.txt", "w")
 
 local dumpableitung = "X Y \n"
 
-for i=1,249 do
- dumpableitung = dumpableitung.. i/10 .." ".. ph(i) .. "\n"
-end
- dumpableitung =  dumpableitung.. 25.0 .." ".. 7 .. "\n"
+for i=1,24 do
+ dumpableitung = dumpableitung.. i .. " & ".. i/10 .." & " .. (i+50) .. " & " .. (2.5 - 0.1*i) .." & "..string.format("%.3f",(2.5 - 0.1*i)/(i+50)) .." & "..  ph(i+50) .. "\\\\\n"
+ --dumpableitung = dumpableitung..  i .." "..  ph(i+50) .. "\n"
 
-for i=251,300 do
-dumpableitung = dumpableitung.. i/10 .." ".. ph2(i) .. "\n"
+end
+ dumpableitung = dumpableitung.. 25 .. " & ".. 2.5 .." & " .. 75 .. " & " .. 0 .." & ".. 0 .." & "..  7 .. "\\\\\n"
+--dumpableitung = dumpableitung..  25 .. " ".. 7 .. "\n"
+
+for i=26,50 do
+ dumpableitung = dumpableitung.. i .. " & ".. i/10 .." & " .. (i+50) .. " & " .. ( 0.1*(i-25)) .." & "..string.format("%.3f",(0.1*(i-25))/(i+50)) .." & "..  ph2(i+50) .. "\\\\\n"
+ --dumpableitung = dumpableitung..  i .. " ".. ph2(i+50) .. "\n"
+
  -- print(i+25,ph2(i))
 end
 f:write(dumpableitung)
