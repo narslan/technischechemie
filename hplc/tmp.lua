e3ea1a1a5c1896f0fe397a92ffe83bf95d4aa038
Nevroz Arslan <newrozarslan@gmail.com>
2016-12-11 23:24:09 +0100

    ddsds

--- hplc/tmp.lua
+++ hplc/tmp.lua
@@ -22,7 +22,7 @@ end
 
 
 --- SMDE,
-for line in io.lines("gc.csv") do
+for line in io.lines("coffein.csv") do
   table.insert(lines, line)
 end
 local summex=0
@@ -89,9 +89,12 @@ local sb=syx*math.sqrt((1/n)+(xmean^2/xminxmean))
 local sm=math.sqrt(syx^2/xminxmean)
 local sx0=syx/m  --- Verfahrungsstandartabweichung
 local Vxo=sx0*100/xmean ---die relative Verfahrungsstandartabweichung
+print(m)
+print(b)
 print(sb)
+print(sx0)
 print(Vxo)
-
+print(xmean)
 
 ---
 function mess(messwert)
@@ -153,73 +156,5 @@ for i=1,n do
 end
 
 
-function drawD()
-    tex.sprint("\\addplot[".."only marks".."] coordinates{")
-    -- tex.sprint("\\addplot coordinates{")
-    for i=1,n do
-      tex.sprint("("..xwerte[i]..","..ywerte[i]..")")
-    end
-    tex.sprint("};")
- --   tex.sprint("\\addplot[".."domain="..minx..":"..maxx..", no markers".."]{"..quad.a.."*x^2+"..quad.b.."*x+"..quad.c.."};")
-    tex.sprint("\\addplot[".."domain="..minx..":"..maxx..", no markers".."]{"..m.."*x+"..b.."};")
---   tex.sprint("\\addplot[only marks,color=red] coordinates{(".."0"..","..(-b/m)..")};")
-
---   tex.sprint("\\node[small dot,pin=90:{"..round(math.abs(-b/m),2).."}] at (axis description cs:0.182,0) {};")
-
-   -- tex.sprint("\\addplot[only marks] coordinates {("..nwg..","..mess(nwg)..")};")
-
-end
-local probe1=mess(7781.223)
-
-function tabelle()
-  if b>0 then
-    vz='+'..b
-    else
-    vz='+'..b
-  end
-  tex.print("\\begin{table}[ht!]\\centering \\begin{tabularx}{\\textwidth}{lll}")
-  tex.print("$y$ & $ \\hat{y} $ & $ \\sum y^2$ \\\\ \\hline ")
-  for i = 1,#ywerte do
-    tex.print(ywerte[i]   .. " & " .. mess(xwerte[i]) .. " & " ..tsummeyy[i] .. "\\\\ \\hline ")
-  end
-  tex.print("\\end{tabularx}  \\caption{Bestimmung des Fehlers} \\end{table} ")
 
 
-
-  tex.print('\\Large {\\textbf{y='..string.format('%.4f',m)..'*x'..string.format('%.4f',vz)..'}}')
-  tex.print('\\par')
-
-tex.print('Standartabweichung:'..string.format('%.4f',sx0))
-tex.print('\\par')
---tex.print('relative Standardabweichung des Verfahrens:'..string.format('%.4f',Vxo)..'\\%')
---tex.print('\\par')
-tex.print('Nachweisgrenze:'..string.format('%.4f',nwg)..'mg/L')
-tex.print('\\par')
-tex.print('Bestimmungsgrenze:'..string.format('%.4f',xbg)..'mg/L')
-tex.print('\\par')
-tex.print('Probe1='..string.format('%.4f',probe1)..'mg/L')
---tex.print('Probe2='..string.format('%.4f',probe2)..'mg/L')
---tex.print('\\par')
-
---for i=1,n do
---tex.sprint(operator(xwerte[i])..'\\par')
---end
-
-
---local sb=syx*math.sqrt((1/n)+(xmean^2/xminxmean))
---local sm=math.sqrt(syx^2/xminxmean)
---local sx0=syx/m  --- Verfahrungsstandartabweichung
---local Vxo=sx0*100/xmean ---die relative Verfahrungsstandartabweichung
-
-
-
-local kka=string.format('%.4f',quad.a)
-local kkb=string.format('%.4f',quad.b)
-local kkc=string.format('%.4f',quad.c)
-
-
-
---tex.print('{\\color{scarletred1}\\textbf{Quadratische Kalibrierkurve='..kka..'*$x^2$+'..kkb..'$*x+$'..kkc..' }}')
---tex.print('Quadratische Standartabweichung='..math.sqrt(string.format('%.4f',summy/n)))
---tex.print('\\node [ draw,anchor=west] (kalibrier) at(0,0){\\textbf{Kalibrierkurve:y='..m..'*x'..vz..'}};')
-end
